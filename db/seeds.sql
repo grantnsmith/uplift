2aa4e311c05d56ab8ab26f91385767adb0bb774f
Armenuhi <avaarm95@gmail.com>
2020-06-06 09:19:17 -0700

    adding db files and example readme.md and license

---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES ("Alex","Honnold",1,2);
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES ("Chris", "Sharma",2 ,3);
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES ("Alex", "Puccio",3 ,2);
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES ("Lynn", "Hill",2 ,3);
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES ("Adam", "Ondra",1,1);
+
+INSERT INTO role (title,salary,department_id)
+VALUES ("Accountant", 50000, 1);
+INSERT INTO role (title,salary,department_id)
+VALUES ("Sales Manager", 90000, 2);
+INSERT INTO role (title,salary,department_id)
+VALUES ("VP", 250000, 1);
+INSERT INTO role (title,salary,department_id)
+VALUES ("President", 290000, 3);
+INSERT INTO role (title,salary,department_id)
+VALUES ("Analyst", 100000, 2);
+
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Sales Manager");
+INSERT INTO department (name)
+VALUES ("Executive");
+INSERT INTO department (name)
+VALUES ("Executive");
+INSERT INTO department (name)
+VALUES ("Finance");
