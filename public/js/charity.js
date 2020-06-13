c1391c0618e2b110de6b4bc9d653776467cbc908
Armenuhi <avaarm95@gmail.com>
2020-06-13 09:38:23 -0700

    business js added for adding to database

--- public/js/charity.js
+++ public/js/charity.js
@@ -1,19 +1,40 @@
- const axios = require('axios')
- // could web scrape and use puppeteer
-    // could parse the html string and pull out all li tags 
+/* eslint-disable indent */
+/* eslint-disable no-var */
+/* eslint-disable prettier/prettier */
+/* eslint-disable semi */
+ const axios = require("axios")
+
 
 // POST request 
 
+
+// we want to get the data array out of this function
+// then we will iterate over it to get the values we need
+
 function getApiCall() {
-  var Key = `secretkey`
-  axios.post(`http://data.orghunter.com/v1/categories?user_key=${Key}`)
-  .then(function(response){console.log(response.data)})
-  .catch(function(error){console.log(error)});
-}
+  var Key = "2a07eff0f6fef1c4db7b8e5ec35d37ea";
+  return axios.post(`http://data.orghunter.com/v1/categories?user_key=${Key}`)
+  .then((response) => {console.log(response.data)})
+  .catch((error) => {console.log(error)});
+  }
+ 
+getApiCall();
 
 
-getApiCall()
 
 
-///node public/js/charity
-// linter is so bad
+// async function makeApiCall() {
+//   var Key = "2a07eff0f6fef1c4db7b8e5ec35d37ea";
+//   const res = await axios.post(`http://data.orghunter.com/v1/categories?user_key=${Key}`);
+//   const data = res.data;
+//   return data;
+// }
+
+// function getCategoryById(categoryId) {
+//   data = makeApiCall();
+//   data.then(value => {return value[categoryId];});
+// }
+
+
+// categoryDescriptionForR = getCategoryById("R");
+// console.log(categoryDescriptionForR);
